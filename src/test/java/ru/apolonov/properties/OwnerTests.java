eb47735175f28f9f25ddd64b1450191fb6b61fe7
vasvap <vasvap@gmail.com>
2021-11-27 23:06:48 +0300

    added OwnerTests

---
+++ src/test/java/ru/apolonov/properties/OwnerTests.java
@@ -0,0 +1,25 @@
+package ru.apolonov.properties;
+
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+import ru.apolonov.config.CredentialsConfig;
+
+import static java.lang.String.format;
+
+public class OwnerTests {
+
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    void readCredentialsTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+
+        String message = format("i login as %s with password %s", login, password);
+        System.out.println(message);
+    }
+}
