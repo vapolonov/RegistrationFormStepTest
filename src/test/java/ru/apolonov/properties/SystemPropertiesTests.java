1a36eb3ad24a9c54f67660734dadd0553b07b89b
vasvap <vasvap@gmail.com>
2021-11-27 18:19:37 +0300

    added SystemPropertiesTests

--- src/test/java/ru/apolonov/properties/SystemPropertiesTests.java
+++ src/test/java/ru/apolonov/properties/SystemPropertiesTests.java
@@ -1,4 +1,33 @@
 package ru.apolonov.properties;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 public class SystemPropertiesTests {
+
+    @Test
+    @Tag("properties")
+    void someTest4() {
+        String value = System.getProperty("browser", "chrome");
+        System.out.println(value);
+        //  gradle clean properties_tests
+        //  chrome
+
+        //  gradle clean properties_tests -Dbrowser=opera
+        //  opera
+    }
+
+    @Test
+    @Tag("properties")
+    void someTest5() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+
+        // gradle clean properties_tests -Dbrowser=opera -Dversion=93
+    }
 }
