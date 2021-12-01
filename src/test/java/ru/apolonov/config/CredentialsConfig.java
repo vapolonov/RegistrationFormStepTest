d9bc6b374192a811ee712c483e655023b0beaa42
Vasiliy <vasvap@gmail.com>
2021-12-01 15:16:04 +0300

    classpath changed

--- src/test/java/ru/apolonov/config/CredentialsConfig.java
+++ src/test/java/ru/apolonov/config/CredentialsConfig.java
@@ -2,7 +2,7 @@ package ru.apolonov.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:config/credentials.properties"})
+@Config.Sources({"classpath:resources/config/credentials.properties"})
 public interface CredentialsConfig extends Config {
     String login();
     String password();
