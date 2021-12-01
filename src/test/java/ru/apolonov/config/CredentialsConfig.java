29b8818224d8a060f241ec1160ba1dbe4ea3b6fa
Vasiliy <vasvap@gmail.com>
2021-12-01 15:23:27 +0300

    fix error

--- src/test/java/ru/apolonov/config/CredentialsConfig.java
+++ src/test/java/ru/apolonov/config/CredentialsConfig.java
@@ -2,7 +2,7 @@ package ru.apolonov.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:resources/config/credentials.properties"})
+@Config.Sources({"classpath:config/credentials.properties"})
 public interface CredentialsConfig extends Config {
     String login();
     String password();
